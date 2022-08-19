46aa44560c51ab576e9e142a3823ed4756857ebb
carlos <carlos.caserotto@yahoo.com>
2022-08-19 14:34:44 -0300

    added post actions and views; new, create, update, destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+  root 'books#index'
  
   get 'catalog/index'
   get 'catalog/show/:id', to: 'catalog#show'
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   post 'cart/:id', to: 'cart#add'
   get 'cart/dell/:id', to: 'cart#dell'
   
-  
+  resources :posts
   
   resources :books
   resources :publishers
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_12_114644) do
+ActiveRecord::Schema.define(version: 2022_08_19_133837) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -76,6 +76,13 @@ ActiveRecord::Schema.define(version: 2022_08_12_114644) do
   end
 
   create_table "carts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
